ab3a39754b1fdd2645f4e4071fb9a2b4958c16ba
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-31 17:44:03 -0800

    Build workout model

--- app/models/workout.js
+++ app/models/workout.js
@@ -2,15 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  type: String,
-  name: String,
-  distance: Number,
-  duration: Number,
-  weight: Number,
-  sets: Number,
-  reps: Number
-});
+const WorkoutSchema = new Schema({});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
